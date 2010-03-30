d14fb7c3dd193beaec52679718b7c7f14913bbfe
Will Maier <willmaier@ml1.net>
2010-03-29 20:37:40 -0500

    wsgirest -> neat.
    --HG--
    rename : lib/project/__init__.py => lib/neat/__init__.py
    rename : lib/project/_ext/__init__.py => lib/neat/_ext/__init__.py
    rename : lib/project/ext.py => lib/neat/ext.py
    rename : wsgirest.py => lib/neat/neat.py

--- lib/neat/__init__.py
+++ lib/neat/__init__.py
@@ -2,23 +2,25 @@
 %PROJECT%
 """
 
-__project__ = "%PROJECT%"
+__project__ = "neat"
 __version__ = "0.1"
-__package__ = "project"
-__description__ = ""
-__author__ = ""
-__author_email__ = ""
-__url__ = ""
+__package__ = "neat"
+__description__ = "neat WSGI API framework"
+__author__ = "Will Maier"
+__author_email__ = "willmaier@ml1.net"
+__url__ = "http://code.lfod.us/neat"
 
 # See http://pypi.python.org/pypi?%3Aaction=list_classifiers.
 __classifiers__ = [] 
 __keywords__ = ""
 
-__requires__ = []
+__requires__ = [
+	"WebOb",
+]
 
 # The following is modeled after the ISC license.
 __copyright__ = """\
-Copyright (c) CCYY YOUR NAME HERE <user@your.dom.ain>
+Copyright (c) 2010 Will Maier <willmaier@ml1.net>
 
 Permission to use, copy, modify, and distribute this software for any
 purpose with or without fee is hereby granted, provided that the above
