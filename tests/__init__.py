f546ae20f7001535e00c3dacb36aefebcd17cbb9
Will Maier <willmaier@ml1.net>
2010-04-15 15:55:07 -0500

    Add a handy application tester.

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,5 +3,15 @@ try:
 except ImportError:
     import unittest
 
+from webob import Request, Response
+
 class BaseTest(unittest.TestCase):
     pass
+
+class AppTest(BaseTest):
+    application = None
+    
+    def app(self, *args, **kwargs):
+        req = Request.blank(*args, **kwargs)
+
+        return req.get_response(self.application)
