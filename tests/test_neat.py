606e3a48aa3633a0eb75259043bf3765e68ae03f
Will Maier <willmaier@ml1.net>
2010-04-16 10:34:26 -0500

    Test what happens when we have multiple matches.

--- tests/test_neat.py
+++ tests/test_neat.py
@@ -14,9 +14,24 @@ class TestResource(AppTest):
             def list(self):
                 pass
 
+        class Multiple1(Resource):
+            collection = "multiple"
+            mimetypes = {"*/*": "text"}
+
+            def retrieve_text(self, req, member):
+                return "multiple1: %s" % member
+
+        class Multiple2(Resource):
+            collection = "multiple"
+
+            def retrieve_text(self, req, member):
+                return "multiple2: %s" % member
+
         service = Service(
             Empty(),
             Minimal(),
+            Multiple1(),
+            Multiple2(),
         )
 
         self.application = service
@@ -28,3 +43,7 @@ class TestResource(AppTest):
     def test_no_method(self):
         response = self.app("/empty")
         self.assertEqual(response.status_int, 404)
+
+    def test_multiple_matches(self):
+        response = self.app("/multiple/foo")
+        self.assertEqual(response.body, "multiple1: foo")
